fcbe1cb054dc762b595ae30ed5f88d73748e74b7
Jay Pal <contactjaypal@gmail.com>
2021-12-27 00:56:01 +0530

    add bio and gravatar

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use GrahamCampbell\Markdown\Facades\Markdown;
 
 class User extends Authenticatable
 {
@@ -45,6 +46,21 @@ class User extends Authenticatable
     public function posts()
     {
         return $this->hasMany(Post::class, 'author_id');
+    }
+
+    public function gravatar()
+    {
+        $email = $this->email;
+        //$default = asset('img/author.jpg');
+        $default = 'https://upload.wikimedia.org/wikipedia/commons/9/99/Sample_User_Icon.png';
+        $size = 100;
+        return "https://www.gravatar.com/avatar/" . md5( strtolower( trim( $email ) ) ) . "?d=" . urlencode( $default ) . "&s=" . $size;
+
+    }
+
+    public function getBioHtmlAttribute($value)
+    {
+        return $this->bio ? Markdown::convertToHtml(e($this->bio)): NULL;
     }
 
     public function getRouteKeyName()
